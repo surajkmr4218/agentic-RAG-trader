683a9b69b7ccb704d97c1ea7d0b3191e545d16c1
Suraj Kumar <surajkmr4218@gmail.com>
2026-06-17 19:06:49 -0700

    pass CI by ignoring alembic migrations

--- app/config.py
+++ app/config.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
